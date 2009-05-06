bde21a11d070ec19242d751268638293d6555882
hlb <hlb@handlino.com>
2009-05-06 18:43:43 +0800

    import jquery.easing, jquery.galleryview and apply it

---
+++ content/js/application.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('#photos').galleryView({
+        panel_width: 450,
+        panel_height: 300,
+        frame_width: 66,
+        frame_height: 40,
+        panel_scale: 'crop',
+        pointer_size: 4
+    });
+});
